d2bd3c9dad4c1bfe727ae01540ea5180127d38b5
Huguez <carlos.huguez@gmail.com>
2020-07-28 19:44:00 -0700

    se agrego el icono de hamburguesa

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,7 +4,7 @@ import classes from './Toolbar.css';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from "../NavigationItems/NavigationItems";
-import DrawerToggle from '../SideDrawer/DrawerToggle.js/DrawerToggle';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = ( props ) => ( 
     <header className={ classes.Toolbar } >
