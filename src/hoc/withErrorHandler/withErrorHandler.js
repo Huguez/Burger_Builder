3838ae099e66dd1207ef04dc91a8496b9df7520a
Huguez <carlos.huguez@gmail.com>
2020-08-02 00:02:20 -0700

    se maneja el caso de los request y response invalidos

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -12,16 +12,21 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
             error: null
         }
 
-        UNSAFE_componentWillMount(  ){
+        UNSAFE_componentWillMount(){
             
-            axios.interceptors.response.use( req => { 
+            this.reqInterceptor = axios.interceptors.response.use( req => { 
                 this.setState( { error: null } );
                 return req;
             });
 
-            axios.interceptors.response.use( res => res, error => { 
+            this.resInterceptor = axios.interceptors.response.use( res => res, error => { 
                 this.setState( { error: error } )
             });
+        }
+
+        componentWillUnmount(){
+            axios.interceptors.request.eject( this.reqInterceptor );
+            axios.interceptors.response.eject( this.resInterceptor );
         }
 
         errorConfirmed = () => {
