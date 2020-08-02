132385288504988eba8a4614d14123a157461e5a
Huguez <carlos.huguez@gmail.com>
2020-08-01 21:29:01 -0700

    se modifico el modal para mostrar resultados

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,10 +5,10 @@ import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
     
-    // shouldComponentUpdate( nextProps, nextState ){
-    //     console.log("shouldComponentUpdate ");
-    //     return nextProps.show !== this.props.show;
-    // }
+    shouldComponentUpdate( nextProps, nextState ){
+        // console.log("shouldComponentUpdate ");
+        return nextProps.show !== this.props.show  ||  nextProps.children !== this.props.children;
+    }
 
     // UNSAFE_componentWillUpdate(){
     //     console.log("simon ");
