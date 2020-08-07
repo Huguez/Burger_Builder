4df77e43cf949ddefacc6ff98f52bb1e872f4113
Huguez <carlos.huguez@gmail.com>
2020-08-06 18:31:16 -0700

    se creo una pagina nueva para hacer la redireccion

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Burger from '../../Burger/Burger';
+import Button from "../../UI/Button/Button";
+import classes from './CheckoutSummary.css';
+
+const CheckoutSummary =(props) =>{
+    
+    return (
+        <div className={ classes.CheckoutSummary }>
+            <h1> Buen provecho bato !!! </h1>
+            <div style={{ width:'100%', height: '300px', margin: 'auto' }}>
+                <Burger ingredientes={ props.ingredients } />
+            </div>
+            <Button btnType="Danger"  clicked={props.onCheckoutCancelled} >Cancel</Button>
+            <Button btnType="Success" clicked={props.onCheckoutContinue}  >Continue</Button>
+        </div>
+    );
+}
+
+export default CheckoutSummary;
