1c4182c4c221052178af45a5994f7fc934f8cb9e
Huguez <carlos.huguez@gmail.com>
2020-09-30 00:17:59 -0700

    converting orderSummary, buergerIngredientes with hooks and modalwith React.memo

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,49 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import clases from './BurgerIngredients.css'
 import propTypes from 'prop-types';
 
+const burguerIngredients = props => {
 
-class BurguerIngredients extends Component {
-
-    render(){
-        let ingrediente = null;
-        
-        switch( this.props.type ){
-            case ('bread-bottom'):
-                ingrediente = <div className={ clases.BreadBottom }></div>
-                break;
-            case ('bread-top'):
-                ingrediente = (
-                    <div className={ clases.BreadTop }>
-                        <div className={ clases.Seeds1 }></div>
-                        <div className={ clases.Seeds2 }></div>
-                    </div>
-                );
-                break;
-            case ('meat'):
-                ingrediente = <div className={ clases.Meat }></div>
-                break;
-            case ('bacon'):
-                ingrediente = <div className={ clases.Bacon }></div>
-                break;
-            case ('salad'):
-                ingrediente = <div className={ clases.Salad }></div>
-                break;
-            case ('cheese'):
-                ingrediente = <div className={ clases.Cheese }></div>
-                break;
-            default:
-                ingrediente = null;
-                break;
-        }
-        return ingrediente;
+    let ingrediente = null;
+    
+    switch( props.type ){
+        case ('bread-bottom'):
+            ingrediente = <div className={ clases.BreadBottom }></div>
+            break;
+        case ('bread-top'):
+            ingrediente = (
+                <div className={ clases.BreadTop }>
+                    <div className={ clases.Seeds1 }></div>
+                    <div className={ clases.Seeds2 }></div>
+                </div>
+            );
+            break;
+        case ('meat'):
+            ingrediente = <div className={ clases.Meat }></div>
+            break;
+        case ('bacon'):
+            ingrediente = <div className={ clases.Bacon }></div>
+            break;
+        case ('salad'):
+            ingrediente = <div className={ clases.Salad }></div>
+            break;
+        case ('cheese'):
+            ingrediente = <div className={ clases.Cheese }></div>
+            break;
+        default:
+            ingrediente = null;
+            break;
     }
+
+    return ingrediente;
 }
 
-BurguerIngredients.propTypes = { 
+burguerIngredients.propTypes = { 
     type: propTypes.string.isRequired
 };
 
-
-
-export default BurguerIngredients;
+export default burguerIngredients;
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button'
 
+const orderSummary = props => {
 
+    const ingredientSummary =  Object.keys( props.ingredientes ).map( 
+        igKey => {
+            return( 
+                <li key={ igKey }>
+                    <span style={ { textTransform: 'capitalize' } } >
+                        { igKey }
+                    </span>: { props.ingredientes[igKey] }
+                </li>)
+        }
+    );
 
-class OrderSummary extends Component{
-    
-    render(){
-        const ingredientSummary =  Object.keys( this.props.ingredientes ).map( 
-            igKey => {
-                return( 
-                    <li key={ igKey }>
-                        <span style={ { textTransform: 'capitalize' } } >
-                            { igKey }
-                        </span>: { this.props.ingredientes[igKey] }
-                    </li>)
-            }
-        );
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p> A la Burguer!!! </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p> <strong> Total Price: { props.price }  </strong>  </p>
+            <p> Continue to Checkout? </p>
+            <Button clicked={ props.purchaseCanceled } btnType="Danger">Cancel</Button>
+            <Button clicked={ props.purchaseContinued } btnType="Success">Continue</Button>
+        </Aux>
+    );
+}
 
-        return (
-            <Aux>
-                <h3>Your Order</h3>
-                <p> A la Burguer!!! </p>
-                <ul>
-                    {ingredientSummary}
-                </ul>
-                <p> <strong> Total Price: { this.props.price }  </strong>  </p>
-                <p> Continue to Checkout? </p>
-                <Button clicked={ this.props.purchaseCanceled } btnType="Danger">Cancel</Button>
-                <Button clicked={ this.props.purchaseContinued } btnType="Success">Continue</Button>
-            </Aux>
-        );
-    }
-} 
-export default OrderSummary;
+export default orderSummary;
